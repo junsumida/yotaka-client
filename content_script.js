4e451659c87c62c445494a9e358fc5a6efb43804
Jun Sumida <sekitoh.rh@gmail.com>
2012-10-12 15:35:07 +0900

    foo-bar

--- content_script.js
+++ content_script.js
@@ -1,6 +1,21 @@
+/*global $ */
 console.log("external execution!");
 var html = document.getElementsByTagName("html");
 console.log(typeof html);
 console.log(html[0]);
 console.log(html[0].toString());
 console.log(html[0].innerHTML);
+console.log(typeof $);
+$(document).mousemove(function(e){
+	window.mouseX = e.pageX;
+	window.mosueY = e.pageY;
+	console.log(e.pageX);
+});
+/*
+function showMouseOffset(){
+	console.log("X:"+window.mouseX+" Y:"+window.mouseY);
+	setTimeout(showMouseOffset, 1000);
+}
+
+showMouseOffset();
+*/
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,8 @@
 /*global $,chrome */
 $(document).ready(function(){
 	$("#record").click(function(){
-		chrome.tabs.executeScript(null, {file: "content_script.js"});
+		chrome.tabs.executeScript(null, {file: "./js/jquery.js"}, function(){
+			chrome.tabs.executeScript(null, {file: "content_script.js"});
+		});
 	});
 });
